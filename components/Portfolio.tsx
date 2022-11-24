c08c20507db0745a2a24f538ab9449a96881eb80
Byron Lewis <b.lewis.development@gmail.com>
2022-11-24 21:45:35 +1100

    image path fix

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -67,7 +67,7 @@ export const Portfolio = () => {
 							<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
 								<img
 									className="object-cover w-full h-56 md:h-64 xl:h-80"
-									src="TTlogo.jpg"
+									src="TTLogo.jpg"
 									alt=""
 								/>
 								<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
