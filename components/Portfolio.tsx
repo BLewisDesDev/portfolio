d2250bdc6233ed636e6a8cd7a227d08a966a438c
Byron Lewis <b.lewis.design.development@gmail.com>
2022-12-02 09:08:34 +1100

    Stagger Animations Added

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -5,16 +5,13 @@ import { useInView } from "react-intersection-observer";
 import Link from "next/link";
 
 export const Portfolio = () => {
-	const { ref, inView, entry } = useInView({
-		threshold: 0.8,
+	const { ref, inView } = useInView({
+		threshold: 0.3,
+		triggerOnce: true,
 	});
+
 	return (
 		<>
-			{/* <div ref={ref}>
-				<h2>{`Header inside viewport ${inView}.`}</h2>
-			</div> */}
-
-			{/* Content 2 */}
 			<section>
 				<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
 					<div className="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
@@ -27,80 +24,109 @@ export const Portfolio = () => {
 						</h2>
 					</div>
 					<div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
-						<Link href="/projects" aria-label="View Item">
-							<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-								<img
-									className="object-cover w-full h-56 md:h-64 xl:h-80"
-									src="/readybank.png"
-									alt="Readybank"
-								/>
-								<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-									<p className="mb-4 text-lg font-bold text-gray-100">
-										Ready Bank
-									</p>
-									<p className="text-sm tracking-wide text-gray-300">
-										Social Media site with banking features. Built as a
-										prototype for my major project in my final semester at
-										university.
-									</p>
+						<motion.div
+							ref={ref}
+							initial={{ opacity: 0, y: -20 }}
+							animate={inView ? { opacity: 1, y: 0 } : { opacity: 0 }}
+							transition={{ duration: 0.5 }}
+						>
+							<Link href="/projects" aria-label="View Item">
+								<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+									<img
+										className="object-cover w-full h-56 md:h-64 xl:h-80"
+										src="/readybank.png"
+										alt="Readybank"
+									/>
+									<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+										<p className="mb-4 text-lg font-bold text-gray-100">
+											Ready Bank
+										</p>
+										<p className="text-sm tracking-wide text-gray-300">
+											Social Media site with banking features. Built as a
+											prototype for my major project in my final semester at
+											university.
+										</p>
+									</div>
 								</div>
-							</div>
-						</Link>
-						<Link href="/projects" aria-label="View Item">
-							<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-								<img
-									className="object-cover w-full h-56 md:h-64 xl:h-80"
-									src="osli.png"
-									alt=""
-								/>
-								<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-									<p className="mb-4 text-lg font-bold text-gray-100">OSLi</p>
-									<p className="text-sm tracking-wide text-gray-300">
-										An open source sample library used to organise sound files
-										and samples. OSLi will provide a a free alternative to the
-										Splice sample library plugin that can be used with any
-										existing file structure. Plans to extend file type
-									</p>
+							</Link>
+						</motion.div>
+						<motion.div
+							ref={ref}
+							initial={{ opacity: 0, y: -20 }}
+							animate={inView ? { opacity: 1, y: 0 } : { opacity: 0 }}
+							transition={{ duration: 0.5, delay: 0.2 }}
+						>
+							<Link href="/projects" aria-label="View Item">
+								<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+									<img
+										className="object-cover w-full h-56 md:h-64 xl:h-80"
+										src="osli.png"
+										alt=""
+									/>
+									<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+										<p className="mb-4 text-lg font-bold text-gray-100">OSLi</p>
+										<p className="text-sm tracking-wide text-gray-300">
+											An open source sample library used to organise sound files
+											and samples. OSLi will provide a a free alternative to the
+											Splice sample library plugin that can be used with any
+											existing file structure. Plans to extend file type
+										</p>
+									</div>
 								</div>
-							</div>
-						</Link>
-						<Link href="/projects" aria-label="View Item">
-							<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-								<img
-									className="object-cover w-full h-56 md:h-64 xl:h-80"
-									src="TalentoTotoalLogo.jpg"
-									alt=""
-								/>
-								<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-									<p className="mb-4 text-lg font-bold text-gray-100">
-										Talento Total
-									</p>
-									<p className="text-sm tracking-wide text-gray-300">
-										A Demo site for the Talento Total education non-profit.
-									</p>
+							</Link>
+						</motion.div>
+						<motion.div
+							ref={ref}
+							initial={{ opacity: 0, y: -20 }}
+							animate={inView ? { opacity: 1, y: 0 } : { opacity: 0 }}
+							transition={{ duration: 0.5, delay: 0.4 }}
+						>
+							<Link href="/projects" aria-label="View Item">
+								<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+									<img
+										className="object-cover w-full h-56 md:h-64 xl:h-80"
+										src="TalentoTotoalLogo.jpg"
+										alt=""
+									/>
+									<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+										<p className="mb-4 text-lg font-bold text-gray-100">
+											Talento Total
+										</p>
+										<p className="text-sm tracking-wide text-gray-300">
+											A Demo site for the Talento Total education non-profit.
+										</p>
+									</div>
 								</div>
-							</div>
-						</Link>
-						<Link href="/projects" aria-label="View Item">
-							<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-								<img
-									className="object-cover w-full h-56 md:h-64 xl:h-80"
-									src="deeplayer.png"
-									alt=""
-								/>
-								<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-									<p className="mb-4 text-lg font-bold text-gray-100">
-										Deeplayer
-									</p>
-									<p className="text-sm tracking-wide text-gray-300">
-										A machine learning model that genereates MIDI chord
-										progressions and melodies. Uses a LSTM neural network to
-										generate notes based on my private dataset of MIDI files.
-									</p>
+							</Link>
+						</motion.div>
+						<motion.div
+							ref={ref}
+							initial={{ opacity: 0, y: -20 }}
+							animate={inView ? { opacity: 1, y: 0 } : { opacity: 0 }}
+							transition={{ duration: 0.5, delay: 0.6 }}
+						>
+							<Link href="/projects" aria-label="View Item">
+								<div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+									<img
+										className="object-cover w-full h-56 md:h-64 xl:h-80"
+										src="deeplayer.png"
+										alt=""
+									/>
+									<div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+										<p className="mb-4 text-lg font-bold text-gray-100">
+											Deeplayer
+										</p>
+										<p className="text-sm tracking-wide text-gray-300">
+											A machine learning model that genereates MIDI chord
+											progressions and melodies. Uses a LSTM neural network to
+											generate notes based on my private dataset of MIDI files.
+										</p>
+									</div>
 								</div>
-							</div>
-						</Link>
+							</Link>
+						</motion.div>
 					</div>
+
 					<div className="text-center">
 						<Link
 							href="/projects"
