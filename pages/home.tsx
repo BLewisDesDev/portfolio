5d5e0dfbaa295fee96c0a1862a84e778e975d026
Byron Lewis <b.lewis.design.development@gmail.com>
2022-12-07 14:32:04 +1100

    Loading spinners
    Added loading indicators to splash page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,17 +1,15 @@
-import { useState, useEffect, Suspense } from "react";
+// import { useState, useEffect, Suspense } from "react";
 import { motion } from "framer-motion";
 import Head from "next/head";
-import Image from "next/image";
+// import Image from "next/image";
 import { Nav } from "../components/Nav";
 import { Hero } from "../components/Hero";
 import { Portfolio } from "../components/Portfolio";
 import { Footer } from "../components/Footer";
-import gif from "../public/PortfolioSite.gif";
 import { Feature } from "../components/Feature";
+import { ScaleLoader } from "react-spinners";
 
 export default function Home() {
-	const [isIntroReady, setisIntroReady] = useState(false);
-	useEffect(() => {}, []);
 	return (
 		<>
 			<Head>
@@ -20,38 +18,19 @@ export default function Home() {
 				<link rel="icon" href="/ByronLewisDotDevLogo.png" />
 			</Head>
 			<section>
-				<div className="flex justify-center w-full h-screen">
-					<div className="relative z-0">
-						{/* Gif div */}
-						<motion.div initial={{ opacity: 0 }}>
-							<Suspense fallback={<p>Loading...</p>}>
-								<Image
-									src={gif}
-									alt="gif"
-									quality={100}
-									onLoadingComplete={(e) => setisIntroReady(true)}
-									className="z-0 object-cover w-full h-screen"
-								/>
-							</Suspense>
-						</motion.div>
-
-						{/* Content */}
-						<div className="absolute inset-0 z-10">
-							{isIntroReady && (
-								<motion.div
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1 }}
-									transition={{ duration: 0.5 }}
-								>
-									<Nav />
-									<Hero />
-									<Portfolio />
-									{/* <Feature /> */}
-									<Footer />
-								</motion.div>
-							)}
-						</div>
-					</div>
+				{/* Content */}
+				<div className="absolute inset-0 z-10">
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 0.5 }}
+					>
+						<Nav />
+						<Hero />
+						<Portfolio />
+						<Feature />
+						<Footer />
+					</motion.div>
 				</div>
 			</section>
 		</>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
-import { Suspense } from "react";
 import { motion } from "framer-motion";
 import Head from "next/head";
 import Image from "next/image";
 import gif from "../public/PortfolioSite.gif";
 import { useRouter } from "next/router";
+import ScaleLoader from "react-spinners/ScaleLoader";
 
 export default function Home() {
 	const Router = useRouter();
 	const redirect = () => {
 		setTimeout(() => {
 			Router.push("/home");
-		}, 2800);
+		}, 2600);
+	};
+	const loading = () => {
+		return <ScaleLoader color="#C72C3D" />;
 	};
 	return (
 		<>
@@ -21,26 +24,26 @@ export default function Home() {
 			</Head>
 
 			<>
-				<Suspense fallback={<p>Loading...</p>}>
-					<div className="flex justify-center w-full h-screen">
-						<div className="relative z-0">
-							{/* Gif div */}
-							<motion.div
-								initial={{ opacity: 1 }}
-								animate={{ opacity: 0 }}
-								transition={{ duration: 0.2, delay: 2.6 }}
-							>
-								<Image
-									src={gif}
-									alt="gif"
-									quality={100}
-									onLoadingComplete={(e) => redirect()}
-									className="z-0 object-cover w-full h-screen"
-								/>
-							</motion.div>
-						</div>
+				<div className="flex items-center justify-center w-full h-screen">
+					<div className="absolute">
+						<ScaleLoader className="" height={48} width={6} color="#C72C3D" />
 					</div>
-				</Suspense>
+					<div className="z-0">
+						<motion.div
+							initial={{ opacity: 1 }}
+							animate={{ opacity: 0 }}
+							transition={{ duration: 0.2, delay: 2.6 }}
+						>
+							<Image
+								src={gif}
+								alt="gif"
+								quality={100}
+								onLoadingComplete={(e) => redirect()}
+								className="z-0 object-cover w-full h-screen"
+							/>
+						</motion.div>
+					</div>
+				</div>
 			</>
 		</>
 	);
